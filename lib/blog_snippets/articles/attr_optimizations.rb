358a2e9a8e533ced39fb9c019d8ff8d7aad50ecc
Danny Guinther <dannyguinther@gmail.com>
2015-03-23 07:45:36 -0400

    Relocate articles into their own directory

---
+++ lib/blog_snippets/articles/attr_optimizations.rb
@@ -0,0 +1,2 @@
+require "blog_snippets/articles/attr_optimizations/minimalist_attrs"
+require "blog_snippets/articles/attr_optimizations/excessive_attrs"
--- lib/blog_snippets/attr_optimizations.rb
+++
@@ -1,2 +0,0 @@
-require "blog_snippets/attr_optimizations/minimalist_attrs"
-require "blog_snippets/attr_optimizations/excessive_attrs"
