d512eec24c51627ad924b2c1436a031d04cd5616
Danny Guinther <dannyguinther@gmail.com>
2015-04-10 09:10:28 -0400

    WIP

--- lib/blog_snippets/renderers/wordpress_html_renderer.rb
+++ lib/blog_snippets/renderers/wordpress_html_renderer.rb
@@ -4,6 +4,8 @@ require "json"
 module BlogSnippets
   module Renderers
     class WordpressHTMLRenderer < Redcarpet::Render::HTML
+      # http://rubular.com/r/apmHqN4joc
+      HEADER_MATCHER = /(?<header><h(?<level>[1-6])[^>]+id="(?<id>[^"]+)".*?>.*?<\/h\k<level>>)/.freeze
       INDENTATION_TOKEN = "__WORDPRESS_HTML_RENDERER_INDENTATION__".freeze
       NEW_LINE_TOKEN = "__WORDPRESS_HTML_RENDERER_NEW_LINE__".freeze
 
@@ -30,18 +32,23 @@ module BlogSnippets
       end
 
       def postprocess(document)
-        # Remove line breaks; HTML should handle breaking lines
-        document.gsub!(/\n/, " ")
-        # Removing line breaks may have introduced white space runs; zap 'em.
-        # http://rubular.com/r/aaVCG1Wlep
-        document.gsub!(/(?<=[^\s])\s{2,}/, " ")
-        # Replace tokens with desired characters
-        document.gsub!(/#{NEW_LINE_TOKEN}/, "\n")
-        document.gsub!(/#{INDENTATION_TOKEN}/, "  ")
+        remove_new_lines_and_white_space_runs!(document)
+        replace_tokens!(document)
+        add_header_links!(document)
         document
       end
 
       private
+
+      def add_header_links!(document)
+        document.gsub!(HEADER_MATCHER) do |match|
+          match_data = $~
+          match[0..-6] +
+            %Q|<a href="##{match_data[:id]}"><i class="dashicons dashicons-admin-links"></i></a>| +
+            "</h#{match_data[:level]}>"
+        end
+        document
+      end
 
       def code_attributes(lang_or_attrs)
         return "language=\"#{lang_or_attrs}\"" unless /[, :]/ === lang_or_attrs
@@ -58,6 +65,22 @@ module BlogSnippets
           },
           :with_toc_data => true,
         }
+      end
+
+      def remove_new_lines_and_white_space_runs!(document)
+        # Remove line breaks; HTML should handle breaking lines
+        document.gsub!(/\n/, " ")
+        # Removing line breaks may have introduced white space runs; zap 'em.
+        # http://rubular.com/r/aaVCG1Wlep
+        document.gsub!(/(?<=[^\s])\s{2,}/, " ")
+        document
+      end
+
+      def replace_tokens!(document)
+        # Replace tokens with desired characters
+        document.gsub!(/#{NEW_LINE_TOKEN}/, "\n")
+        document.gsub!(/#{INDENTATION_TOKEN}/, "  ")
+        document
       end
     end
   end
